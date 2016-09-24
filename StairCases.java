99940abc1e3b2e59ae5c75260022f684168b9e0d
Drew Beckmen <dbeckmen>
2016-09-24 19:17:49 -0400

    Improved documentation and formatting

--- StairCases.java
+++ StairCases.java
@@ -21,10 +21,14 @@ public class StairCases
     Scanner keyboard = new Scanner(System.in);
     System.out.print("Tell me the length of the longest row in the ascending and descending staircases: ");
     int size = keyboard.nextInt();
-    keyboard.nextLine(); //Clear the command line in terminal. 
-
+    
+    //Clear the command line in terminal. 
+    keyboard.nextLine(); 
+   
     printAscending(size);
+    //Print an extra line for formatting purposes
     System.out.println();
+    
     printDescending(size);
   }
 
@@ -34,18 +38,21 @@ public class StairCases
    * prints directly to screen. returns void
    */
 
-  public static void printAscending(int dimensions)
+  public static void printAscending(int dimensions) //DONT FORGET: dimensions is a new variable so much be given type
   	{
+  		//Counter will be used to make sure inner for loop happens 1 addition time each run through
   		int counter = dimensions;
   		
-  		for (int x = 1; x <= dimensions; x++)
+  		for (int x = 1; x <= dimensions; x++) //Do what is inside of this loop xDimensions
   		{
-  			counter++;
+  			counter++; //So each time we go through loop, inner for loop is gone through 1 more time
   			
+  			//Crucial: Every time you go into this loop, y will reset to inputted dimensions
   			for (int y = dimensions; y < counter; y++)
   			{
   				System.out.print("x ");
   			}
+  			
   			System.out.println();
   		}
   	
@@ -58,10 +65,12 @@ public class StairCases
   public static void printDescending(int dimensions)
   {
   	
+  	//Make counter 1 more than dimensions so that you can subtract 1 each time you go through outer for loop
   	int counter = (dimensions + 1);
+  	
   	for (int x = 1; x <= dimensions; x++)
   	{
-  		counter = (counter - 1); 
+  		counter--; //Subtract one from counter each time so that goes through inner loop 1 less time
   		for (int y = 1; y <= counter; y++)
   		{
   			System.out.print("x ");
