6857cea4f30e532df51ed166ea5682b1d5120a7f
Drew Beckmen <dbeckmen>
2016-09-24 19:03:56 -0400

    First working version.

--- StairCases.java
+++ StairCases.java
@@ -1,33 +1,73 @@
+import java.util.Scanner;
+
 /**
- * Write javadoc for your class here (and remove this comment, or course)
- *
+ * This class represents a program that will print ascending and descending x's according to user
+ * inputted dimensions
+ * @author Drew Beckmen
+ * @author Josh Orndorff
+ * @version 1.0
  */
 
 public class StairCases
 {
   /**
-   * Main method demonstrates the other wo methods by calling each of them.
+   * Main method demonstrates the other two methods by calling each of them.
    * @param args Standard array of commandline String arguments. Not used in this program.
    */
 
   public static void main(String[] args)
   {
-    //TODO change the next line so it actually gets input from the user
-    int size = 4;
+    //Creating instance keyboard of class Scanner
+    Scanner keyboard = new Scanner(System.in);
+    System.out.print("Tell me the length of the longest row in the ascending and descending staircases: ");
+    int size = keyboard.nextInt();
+    keyboard.nextLine(); //Clear the command line in terminal. 
 
     printAscending(size);
+    System.out.println();
     printDescending(size);
   }
 
   /**
-   * Write your javadoc here
+   * printAscending prints the staircase of x's from 1 up to inputted number
+   * @param dimension of staircase
+   * prints directly to screen. returns void
    */
-  public static void printAscending
-  //TODO Finish this method
+
+  public static void printAscending(int dimensions)
+  	{
+  		int counter = dimensions;
+  		
+  		for (int x = 1; x <= dimensions; x++)
+  		{
+  			counter++;
+  			
+  			for (int y = dimensions; y < counter; y++)
+  			{
+  				System.out.print("x ");
+  			}
+  			System.out.println();
+  		}
+  	
+  	}
 
   /**
-   * Write your javadoc here
+   * printDescending prints a staircase of x's starting at inputted number down to x
+   * @param dimensions of staircase
    */
-  public static void printDescending
-  //TODO Finish this method
+  public static void printDescending(int dimensions)
+  {
+  	
+  	int counter = (dimensions + 1);
+  	for (int x = 1; x <= dimensions; x++)
+  	{
+  		counter = (counter - 1); 
+  		for (int y = 1; y <= counter; y++)
+  		{
+  			System.out.print("x ");
+  		
+  		} 
+  		System.out.println();
+  	}
+  }
 }
